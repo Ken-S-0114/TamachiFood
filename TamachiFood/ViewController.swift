edee1f3d2517e6d942a93ab3b1262fa285f045b8
Ken-S-0114 <g031n071@s.iwate-pu.ac.jp>
2019-06-11 22:51:54 +0900

    first commit

---
+++ TamachiFood/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TamachiFood
+//
+//  Created by 佐藤賢 on 2019/06/11.
+//  Copyright © 2019 佐藤賢. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
