aff13f9a72287993d2af61a97ded286a6b0be0b2
Ken-S-0114 <sato.ken@moneyforward.co.jp>
2019-06-13 13:41:41 +0900

    FoodModel仮定義

--- TamachiFood/Sources/UI/SelectFoodViewController.swift
+++ TamachiFood/Sources/UI/SelectFoodViewController.swift
@@ -10,27 +10,9 @@ import Firebase
 import UIKit
 
 class SelectFoodViewController: UIViewController {
-    @IBOutlet weak var label: UILabel!
-    @IBOutlet weak var textField: UITextField!
-    
-    // インスタンス変数
-    var DBRef: DatabaseReference!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // インスタンスを作成
-        DBRef = Database.database().reference()
     }
     
-    @IBAction func add(_ sender: UIButton) {
-        let age: Int = Int(textField.text!)!
-        let data = ["age": age]
-        DBRef.child("user/01").setValue(data)
-        
-        let defaultPlace = DBRef.child("user/01/age")
-        defaultPlace.observe(.value) { (snap: DataSnapshot) in
-            self.label.text = (snap.value! as AnyObject).description
-        }
-    }
 }
