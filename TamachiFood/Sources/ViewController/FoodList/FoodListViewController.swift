d346774753798d8d5d775ab9bbd83e6203efc5ed
Ken-S-0114 <sato.ken@moneyforward.co.jp>
2019-06-14 13:58:22 +0900

    UICollectionViewのプロトコル定義

---
+++ TamachiFood/Sources/ViewController/FoodList/FoodListViewController.swift
@@ -0,0 +1,33 @@
+//
+//  FoodListViewController.swift
+//  TamachiFood
+//
+//  Created by sato.ken on 2019/06/14.
+//  Copyright © 2019 佐藤賢. All rights reserved.
+//
+
+import UIKit
+
+class FoodListViewController: UIViewController {
+    @IBOutlet weak var foodSearchBar: UISearchBar!
+    @IBOutlet weak var evaluationSegmentedControl: UISegmentedControl!
+    @IBOutlet weak var foodCollectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+}
+
+extension FoodListViewController: UICollectionViewDelegate {}
+
+extension FoodListViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        <#code#>
+    }
+}
